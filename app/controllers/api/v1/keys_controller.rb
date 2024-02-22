8d3263d6e61a17a6e42b87c21177a683984537ca
svv2602 <svv2602@gmail.com>
2024-02-22 17:19:25 +0200

    Убраны элементы с низким рейтингом из обработки

--- app/controllers/api/v1/keys_controller.rb
+++ app/controllers/api/v1/keys_controller.rb
@@ -1,37 +1,47 @@
-
 class Api::V1::KeysController < ApplicationController
 
   def show
-    i=0
-   h=[]
-   arr1 = [["Season",1],["Brand",2],['Diameter',3], ["Addon",4],['City',5]]
-   arr2 = [["Season",1],["Brand",2],['Size',3], ["Addon",4],['City',5]]
+    i = 0
+    h = []
+    arr1 = [["Season", 1], ["Brand", 2], ['Diameter', 3], ["Addon", 4], ['City', 5]]
+    arr2 = [["Season", 1], ["Brand", 2], ['Size', 3], ["Addon", 4], ['City', 5]]
 
-   merged_array = combinations_with_sorted_ratings(arr1) + combinations_with_sorted_ratings(arr2)
-   unique_values = merged_array.uniq
-   unique_values.each  do |arr|
-     h << str_hash(arr)
-     i += 1
-   end
+    merged_array = combinations_with_sorted_ratings(arr1) + combinations_with_sorted_ratings(arr2)
+    unique_values = merged_array.uniq
+    unique_values.each do |arr|
+      record = str_hash(arr)
+      h << {keywords: normal_str(record[:keywords]),url: record[:url]}
+      i +=1
+    end
 
     puts "Количество элементов: #{i}"
+
     render json: {
       keyword: h.shuffle
     }
 
   end
 
-
-
-
   private
+  def normal_str(str)
+    keys = ''
+    case rand(1..3)
+    when 1
+      keys = str.downcase
+    when 2
+      keys = str.downcase.capitalize
+    when 3
+      keys = str.capitalize
+    end
+    keys
+  end
 
   def str_hash(tables_with_data)
     # массивы с именами таблиц
     table_copies = []
     # Проходим циклом по таблицам с данными
     tables_with_data.each_with_index do |table, index|
-      table_copy = table+'Copy' # Преобразуем имя таблицы-копии
+      table_copy = table + 'Copy' # Преобразуем имя таблицы-копии
       table_copies << table_copy
       copy_table_to_table_copy_if_empty(table, table_copy)
     end
@@ -39,7 +49,6 @@ class Api::V1::KeysController < ApplicationController
     return keys
 
   end
-
 
   # Копирование таблицы, переменная - объект
   def copy_table_to_table_copy(model, model_copy)
@@ -71,7 +80,7 @@ class Api::V1::KeysController < ApplicationController
   # запись с помощью метода find_and_destroy_random_record,
   # а затем добавляет эту запись в массив result
   def extract_random_records(tables)
-    rez={}
+    rez = {}
     result = []
 
     random_number = rand(1..100)
@@ -83,11 +92,10 @@ class Api::V1::KeysController < ApplicationController
       url_new = "https://prokoleso.ua/ua/shiny/"
     end
 
-
     tables.each do |table_name|
       received_record = find_and_destroy_random_record(table_name)
       record = received_record[:name]
-      record  = [received_record[:ww],received_record[:hh],received_record[:rr]] if table_name == "SizeCopy"
+      record = [received_record[:ww], received_record[:hh], received_record[:rr]] if table_name == "SizeCopy"
 
       url_new += partial_url(table_name, received_record[:url])
 
@@ -97,11 +105,11 @@ class Api::V1::KeysController < ApplicationController
     end
     p url_new
     result.shuffle.join(" ")
-    rez = {keywords:  result.shuffle.join(" "),
-           url: url_new}
+    rez = { keywords: result.shuffle.join(" "),
+            url: url_new }
   end
 
-  def  partial_url(table_name, record_url)
+  def partial_url(table_name, record_url)
     case table_name
     when "CityCopy", "AddonCopy"
       record_url = ""
@@ -111,12 +119,12 @@ class Api::V1::KeysController < ApplicationController
     record_url
   end
 
-  def  partial_name(table_name, record_name)
+  def partial_name(table_name, record_name)
     case table_name
     when "DiameterCopy"
       record_name = 'R' + record_name
     when "SizeCopy"
-      record_name = size_name(record_name[0],record_name[1], record_name[2])
+      record_name = size_name(record_name[0], record_name[1], record_name[2])
     else
       record_name = record_name
     end
@@ -124,23 +132,23 @@ class Api::V1::KeysController < ApplicationController
   end
 
   # обработка вариантов написания размеров
-  def size_name(ww,hh,rr)
+  def size_name(ww, hh, rr)
     result = []
     case rand(1..210)
     when 1..10
       # 2055516
       result << "#{ww}#{hh}#{rr}"
     when 11..20
-      #205/55R16
+      # 205/55R16
       result << "#{ww}/#{hh}R#{rr}"
     when 21..30
-      #205 55R16
+      # 205 55R16
       result << "#{ww} #{hh}R#{rr}"
     when 31..40
-      #205 55р16
+      # 205 55р16
       result << "#{ww} #{hh}р#{rr}"
     when 41..50
-      #205 5516
+      # 205 5516
       result << "#{ww} #{hh}#{rr}"
 
     when 51..60
@@ -199,28 +207,45 @@ class Api::V1::KeysController < ApplicationController
     result
   end
 
-
   def process_string(input_string)
     input_string.gsub("Copy", "")
   end
 
   # Результат массив комбинаций элементов массива с рейтингом по возврастанию
   # arr = [['а',1],['б',2],['в',3]]
+  # def combinations_with_sorted_ratings(arr)
+  #   combinations = []
+  #
+  #   # Сначала сортируем исходный массив по рейтингу
+  #   sorted_arr = arr.sort_by { |item| item[1] }
+  #
+  #   # Затем создаем комбинации первых элементов
+  #   (1..sorted_arr.length).each do |n|
+  #     sorted_arr.combination(n).each do |combo|
+  #       combinations << combo.map(&:first)
+  #     end
+  #   end
+  #
+  #   combinations
+  # end
+
   def combinations_with_sorted_ratings(arr)
     combinations = []
 
     # Сначала сортируем исходный массив по рейтингу
     sorted_arr = arr.sort_by { |item| item[1] }
 
-    # Затем создаем комбинации первых элементов
+    # Затем создаем комбинации первых элементов, исключая те, у которых рейтинг больше 4
     (1..sorted_arr.length).each do |n|
       sorted_arr.combination(n).each do |combo|
+        # Если комбинация содержит только один элемент и его рейтинг больше 4, пропускаем её
+        next if combo.length == 1 && combo[0][1] > 3
+
         combinations << combo.map(&:first)
       end
     end
 
     combinations
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
-# Rails.application.routes.draw do
-#   namespace :api, defaults: { format: :json }  do
-#     namespace :v1 do
-#       resources :keys, only: [:show]
-#     end
-#   end
-# end
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }  do
