0cfe4197d0e0860289375af414d1fbf6d32af7c6
svv2602 <svv2602@gmail.com>
2024-03-09 01:50:31 +0200

    Добавлен endpoint text_line

--- app/controllers/api/v1/text_error_controller.rb
+++ app/controllers/api/v1/text_error_controller.rb
@@ -1,7 +1,10 @@
 
 class Api::V1::TextErrorController < ApplicationController
-
-
+  def text_line
+    result = "СТРОКА"
+    render json: { result: result }
+    puts result
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       get '/questions', to: 'tyre_questions#questions'
       get '/questions_track', to: 'tyre_questions#questions_track'
       get '/questions_diski', to: 'tyre_questions#questions_diski'
+      get '/text_line', to: 'text_error#text_line'
 
     end
 
