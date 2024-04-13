79f5e32241a65bc93ca413c3d73a9756a5b21171
svv2602 <svv2602@gmail.com>
2024-04-13 09:08:15 +0300

    1149884

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -120,7 +120,7 @@ class ExportsController < ApplicationController
 
   def export_xlsx
     count = 20000 # количество выгружаемых записей
-    max_id = 1169890
+    max_id = 1149884
     @selected_records = SeoContentTextSentence.where("sentence_ua = '' and id < ?", max_id)
                                               .order(id: :desc)
                                               .limit(count)
