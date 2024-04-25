0b60cd50a6f32afb0cc18e164c41a7884c912f1f
svv2602 <svv2602@gmail.com>
2024-04-25 06:32:09 +0300

    fix

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -174,7 +174,7 @@ class ExportsController < ApplicationController
   def export_xlsx
     # выгрузка из базы данных записей для дальнейшего перевода в google
     # перевод грузится в этот же файл, и потом, после обработки всех записей таблицы, все файлы грузятся обратно в базу
-    count = 30000 # количество выгружаемых записей
+    count = 50000 # количество выгружаемых записей
     max_id = params[:max].to_i
     # max_id = 1534655
     @selected_records = SeoContentTextSentence
