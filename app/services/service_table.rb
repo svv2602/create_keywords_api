887616d0d12c1af917981e0864ae9b8ae4f3c9bb
svv2602 <svv2602@gmail.com>
2024-05-07 11:30:50 +0300

    fix

--- app/services/service_table.rb
+++ app/services/service_table.rb
@@ -159,8 +159,7 @@ module ServiceTable
       1904728, 1904727, 1904714, 1904532, 1904405, 1904403, 1904402, 1904401,
       1904398, 1904397, 1904395, 1901148, 1901143, 1900561, 1900536, 1834794,
       1834388, 1806943, 1806942, 1806941, 1886964, 1881630, 1678721, 1678720,
-      1956526,     1952635
-
+      1956526, 1952635
 
     ]
     array_id.each do |id|
@@ -184,22 +183,18 @@ module ServiceTable
 
     words = [
       'светил', 'настольн', 'пространств', 'палитр', 'мелод', 'компьют', 'ASUS', 'ноутбук', 'Dell',
-      'мистич','джунгли','тарелк'
+      'мистич', 'джунгли', 'тарелк'
     ]
     query = words.map { |word| "sentence LIKE '%#{word}%'" }.join(" OR ")
     SeoContentTextSentence.where(query).delete_all
 
-
     records = SeoContentTextSentence.where("sentence like ?", "%Предложение:%")
     records.each do |record|
       record.update_columns(sentence: record.sentence.gsub(/^Предложение:/, ""),
-                    sentence_ua: record.sentence_ua.gsub(/^Пропозиція:/, ""))
+                            sentence_ua: record.sentence_ua.gsub(/^Пропозиція:/, ""))
     end
 
     # =======================================
-
-
-
 
     QuestionsBlock.where("question_ua ='' ").delete_all
 
@@ -242,8 +237,6 @@ module ServiceTable
       record.update(sentence: record.sentence.gsub("российского", "украинского"),
                     sentence_ua: record.sentence_ua.gsub("російського", "українського"))
     end
-
-
 
   end
 
